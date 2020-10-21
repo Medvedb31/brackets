df42aaec7ddacdf399af6e343a2711f5e72cc0c9
Dmitry Cheplyanskii <31047691+Medvedb31@users.noreply.github.com>
2020-10-21 16:06:03 +0300

    feat: complate task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  arr = bracketsConfig.map(elem => elem.join(''));
+  for(let i = 0; i < arr.length; i++) {
+    if(str.includes(arr[i])) {
+      str = str.replace(arr[i], "");
+      i = -1;
+    }
+  }
+    
+  return (str === '') ? true : false;
 }
